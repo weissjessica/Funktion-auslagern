b66bec79c39ef061e71be3dedac55c4a922fd51f
Jessica Weiss <Jessica.Weiss@stud.bs-aarau.ch>
2020-08-23 18:41:39 +0200

    Projektdateien hinzufügen.

---
+++ clsFunktion1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class clsFunktion1
+    {
+        public int addition(int Zahl1, int Zahl2)
+        {
+            int Resultat = Zahl1 + Zahl2;
+
+            return Resultat;
+        }
+
+        public int add(int Zahl1, int Zahl2)
+        {
+            return Zahl1 + Zahl2;
+        }
+    }
+}
